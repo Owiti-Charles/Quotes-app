4f6c623bb2dfb18037d280b51a8a0b3bc9a6fb1a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-15 16:46:39 +0300

    Add quotes and quote-detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
