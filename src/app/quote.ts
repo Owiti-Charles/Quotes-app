cd80397fbeb91818ebcde1eabdfc9573a57c31f0
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-16 15:56:00 +0300

    Hide details to toogle on a click event

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,15 @@
+import { ɵBrowserPlatformLocation } from '@angular/platform-browser';
+
 export class Quote {
-  id:number;
-  name:string;
-  quote:string;
-  title:string;
-  author:string;
+
+  showInfo:boolean;
+  constructor(  
+    public id:number,
+    public name:string,
+    public title:string,
+    public quote:string,
+    public author:string)
+    {
+      this.showInfo=false;
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,14 +8,20 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   title = 'welcome to tech quotes';
-  quotes :Quote[]=[
-    {id:1,name:'Charles',title:'Human spirit',quote:'The human spirit must prevail over technology.',author:'Albert Einstein'},
-    {id:2,name:'Wendy',title:'Technology…',quote:'Technology… the knack of so arranging the world that we don’t have to experience it.',author:'Max Frisch'},
-    {id:3,name:'Charles',title:'The great myth',quote:'The great myth of our times is that technology is communication.',author:'Libby Larsen'},
-    {id:3,name:'Charles',title:'Communications tools',quote:'Communications tools don’t get socially interesting until they get technologically boring.',author:'Clay Shirky'},
-    {id:3,name:'Charles',title:'Programs and people',quote:'Programs must be written for people to read, and only incidentally for machines to execute.',author:'Harold Abelson'},
-    {id:3,name:'Charles',title:'Technology and life',quote:'For a list of all the ways technology has failed to improve the quality of life, please press three.',author:'Alice Kahn'},
-  ]
+  quotes:Quote[]=[
+    new Quote (1,'Charles','Human spirit','The human spirit must prevail over technology.','Albert Einstein'),
+    new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch'),
+    new Quote (3,'Charles','The great myth','The great myth of our times is that technology is communication.','Libby Larsen'),
+    new Quote (4,'Charles','Communications tools','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky'),
+    new Quote (5,'Charles','Programs and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson'),
+    new Quote (6,'Charles','Technology and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn'),
+  ];
+  // displayInfo(index){
+  //   this.quotes[index].showInfo = !this.quotes[index].showInfo;
+  // }
+  toggle(index){
+    this.quotes[index].showInfo = !this.quotes[index].showInfo;
+  }
   constructor() { }
 
   ngOnInit() {
