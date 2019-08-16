7f40f1ea142234b1d6414327882d6a016f74df38
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-16 14:58:50 +0300

    Add Delete, Like and Dislike buttons

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'; 
 
 @Component({
   selector: 'app-quote-details',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+  @Input() quoty: Quote;
   constructor() { }
 
   ngOnInit() {
