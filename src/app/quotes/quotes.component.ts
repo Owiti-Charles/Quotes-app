0c9906fa7f8d5b5f8f122b5877f472e166e6d374
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-16 18:49:11 +0300

    Update fonts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,10 +11,10 @@ export class QuotesComponent implements OnInit {
   quotes:Quote[] = [
     new Quote (1,'Charles','Human spirit','The human spirit must prevail over technology.','Albert Einstein'),
     new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch'),
-    new Quote (3,'Charles','The great myth','The great myth of our times is that technology is communication.','Libby Larsen'),
-    new Quote (4,'Charles','Communications tools','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky'),
+    new Quote (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen'),
+    new Quote (4,'Beatrice','Communications tools','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky'),
     new Quote (5,'Charles','Programs and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson'),
-    new Quote (6,'Charles','Technology and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn'),
+    new Quote (6,'Alfred','Technology and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn'),
   ];
   displayInfo(index){
     this.quotes[index].showInfo = !this.quotes[index].showInfo;
