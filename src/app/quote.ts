57664b07b5a179b90ead4120ca19fd4440dbd649
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-16 14:27:58 +0300

    Add and display quotes array

---
+++ src/app/quote.ts
@@ -0,0 +1,7 @@
+export class Quote {
+  id:number;
+  name:string;
+  quote:string;
+  title:string;
+  author:string;
+}
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
   title = 'welcome to tech quotes';
+  quotes :Quote[]=[
+    {id:1,name:'Charles',title:'Human spirit',quote:'The human spirit must prevail over technology.',author:'Albert Einstein'},
+    {id:2,name:'Wendy',title:'Technology…',quote:'Technology… the knack of so arranging the world that we don’t have to experience it.',author:'Max Frisch'},
+    {id:3,name:'Charles',title:'The great myth',quote:'The great myth of our times is that technology is communication.',author:'Libby Larsen'},
+    {id:3,name:'Charles',title:'Communications tools',quote:'Communications tools don’t get socially interesting until they get technologically boring.',author:'Clay Shirky'},
+    {id:3,name:'Charles',title:'Programs and people',quote:'Programs must be written for people to read, and only incidentally for machines to execute.',author:'Harold Abelson'},
+    {id:3,name:'Charles',title:'Technology and life',quote:'For a list of all the ways technology has failed to improve the quality of life, please press three.',author:'Alice Kahn'},
+  ]
   constructor() { }
 
   ngOnInit() {
