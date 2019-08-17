0607c91ecfe22ab255d6addc2a3abd47087b723a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-17 13:16:32 +0300

    display total number of likes and dislikes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ import { ɵBrowserPlatformLocation } from '@angular/platform-browser';
 
 export class Quote {
   showInfo: boolean;
-  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,){
+  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date,public likes:number, public dislikes:number){
       this.showInfo=false;
     }
 }
