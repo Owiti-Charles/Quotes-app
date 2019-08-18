9a9b2bb901f3863326502975697ce1099a5b3b1a
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-18 22:24:54 +0300

    Update highlights directive

--- src/app/highlight-qoute.directive.ts
+++ src/app/highlight-qoute.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightQoute]'
 })
 export class HighlightQouteDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
 
 }
